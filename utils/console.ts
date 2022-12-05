9302563004f7763cade98a3413bea922c833fb92
zhumanov <zhumanov.arman@elvoris.cz>
2022-12-05 20:43:23 +0100

    Added day2

---
+++ utils/console.ts
@@ -0,0 +1,11 @@
+export const writeSolutionSeparatorToConsole = (dayNum: number): void => {
+  console.log(
+    `######################## Day ${dayNum} Solution ########################`
+  )
+}
+
+export const writeEndSeparator = (): void => {
+  console.log(
+    `################################################################`
+  )
+}
